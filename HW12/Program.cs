07dfecf9daac2ff45d1dafa47cbdf0b0d020a810
Sergii Chernyshov <atiska200612@gmail.com>
2023-05-07 22:30:05 +0300

    Добавьте файлы проекта.

---
+++ HW12/Program.cs
@@ -0,0 +1,12 @@
+﻿Queue<string> patints = new Queue<string> ();
+string? tmp;
+TimeSpan interval = new TimeSpan(0, 1, 0);
+while (true)
+{
+    Console.WriteLine("Insert name of patint: ");
+    tmp = Console.ReadLine ();
+    patints.Enqueue(tmp);
+    Console.WriteLine("The doctor is busy Wait a minute...");
+    Thread.Sleep(interval);
+    patints.Dequeue ();
+}
